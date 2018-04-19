ae80a3b58fa80fd006aa57e0390a8ca9e728053b
freggy <riegeryannic@gmail.com>
2018-04-19 02:10:54 +0200

    Check if commit hook works #6

--- src/main/java/de/bergwerklabs/nick/NickManager.java
+++ src/main/java/de/bergwerklabs/nick/NickManager.java
@@ -123,7 +123,7 @@ class NickManager implements NickApi {
   }
 
   private NickProfile createFakeProfile(UUID uuid) {
-    // test 5
+    // test 6
     return new NickProfile(uuid, NickUtil.getUniqueNickName(this.takenNickNames), NickUtil.getRandomSkin());
   }
 }
